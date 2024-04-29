8b8e4d5241203a7cc0a36afcaddf0d734a064ecd
Kirill Nesmeyanov <kirill.nesmeianov@tarkov.com>
2024-04-29 08:28:59 +0300

    Remove unused testing assert method

--- libs/rpc-protocol-jsonrpc/tests/TestCase.php
+++ libs/rpc-protocol-jsonrpc/tests/TestCase.php
@@ -8,15 +8,4 @@ use PHPUnit\Framework\Attributes\Group;
 use PHPUnit\Framework\TestCase as BaseTestCase;
 
 #[Group('php-lsp/rpc-protocol-jsonrpc')]
-abstract class TestCase extends BaseTestCase
-{
-    protected function assertionsEnabled(): bool
-    {
-        try {
-            assert(false);
-            return false;
-        } catch (\Throwable $e) {
-            return true;
-        }
-    }
-}
+abstract class TestCase extends BaseTestCase {}
