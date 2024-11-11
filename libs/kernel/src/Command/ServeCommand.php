d800fabfcaf147e501b7212a093595582e0842cd
Kirill Nesmeyanov <kirill.nesmeianov@tarkov.com>
2024-11-12 00:47:12 +0300

    Improve building logic

--- libs/kernel/src/Command/ServeCommand.php
+++ libs/kernel/src/Command/ServeCommand.php
@@ -42,18 +42,46 @@ final class ServeCommand extends Command
             description: 'The TCP port on which the server is running',
             default: $_SERVER['LSP_PORT'] ?? 0,
         );
+
+        $this->addOption(
+            name: 'env',
+            shortcut: 'e',
+            mode: InputOption::VALUE_OPTIONAL,
+            description: 'Sets an environment name to run the command in',
+            default: $_SERVER['APP_ENV'] ?? 'prod',
+        );
+
+        $this->addOption(
+            name: 'root',
+            shortcut: 'r',
+            mode: InputOption::VALUE_OPTIONAL,
+            description: 'Sets an application root directory',
+            default: \getcwd() ?: '.',
+        );
     }
 
     #[\Override]
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $this->setProcessTitle(\vsprintf('PHP Language Server (tcp://%s:%d)', [
+        $title = \vsprintf('PHP Language Server (tcp://%s:%d)', [
             $input->getOption('addr'),
             $input->getOption('port'),
-        ]));
+        ]);
 
-        $app = new Application();
-        $app->run((int) $input->getOption('port'), (string) $input->getOption('addr'));
+        $output->writeln('Running <info>' . $title . '</info>');
+
+        $this->setProcessTitle($title);
+
+        $app = new Application(
+            env: $input->getOption('env'),
+            debug: false,
+            projectDirectory: $input->getOption('root'),
+        );
+
+        $app->run(
+            port: (int) $input->getOption('port'),
+            host: (string) $input->getOption('addr'),
+        );
 
         return self::SUCCESS;
     }
